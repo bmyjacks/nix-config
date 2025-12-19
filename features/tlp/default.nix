3f18e69ca76df534ef9abcc7cb20b12db8edcf6e
Mingyi BAO <bmyjacks@outlook.com>
2025-12-18 23:22:38 -0600

    Add more tlp customizations

--- features/tlp/default.nix
+++ features/tlp/default.nix
@@ -15,7 +15,23 @@ in
 
   config = lib.mkIf cfg.enable {
     services = {
-      power-profiles-daemon = false;
+      power-profiles-daemon.enable = false;
+
+      tlp = {
+        enable = true;
+        settings = {
+          CPU_SCALING_GOVERNOR_ON_AC = "performance";
+          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+          # Energy Performance Preference (EPP)
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+          CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+          # Battery Charge Thresholds (Crucial for T14 longevity)
+          START_CHARGE_THRESH_BAT0 = 75;
+          STOP_CHARGE_THRESH_BAT0 = 80;
+        };
+      };
     };
   };
 }
