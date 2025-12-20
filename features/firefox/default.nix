a6a22100b559484ae0cbc0eb5ec398616e94cca7
Mingyi BAO <bmyjacks@outlook.com>
2025-12-19 18:13:53 -0600

    Add firefox extensions bitwrden

--- features/firefox/default.nix
+++ features/firefox/default.nix
@@ -12,6 +12,32 @@ in
   config = lib.mkIf config.custom.firefox.enable {
     home-manager.users.${username}.programs.firefox = {
       enable = true;
+      profiles.default = {
+        containersForce = true;
+        search = {
+          force = true;
+          default = "google";
+        };
+
+        # Set default
+        isDefault = true;
+        id = 0;
+      };
+
+      # Install bitwarden
+      policies = {
+        ExtensionSettings = {
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            default_area = "navbar";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            installation_mode = "force_installed";
+            private_browsing = true;
+          };
+        };
+
+        # Disable auto updates
+        ExtensionUpdate = false;
+      };
     };
   };
 }
