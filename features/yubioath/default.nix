7992132bbc5f90564118a767814f11a703033c7b
Mingyi BAO <bmyjacks@outlook.com>
2025-12-27 03:37:26 -0600

    Reformat yubioath

--- features/yubioath/default.nix
+++ features/yubioath/default.nix
@@ -14,6 +14,12 @@ in
   options.custom.${featureName}.enable = lib.mkEnableOption "Enable ${featureName}";
 
   config = lib.mkIf cfg.enable {
+    # udev rule
+    services.udev.packages = [ pkgs.yubikey-personalization ];
+    # smartcard
+    services.pcscd.enable = true;
+
+    # gui
     home-manager.users.${username} = {
       home.packages = with pkgs; [
         yubioath-flutter
